0a514f1b0e70b85f8fb0fed3b03125d29b673133
Azimjon Xolmirzayev <155216975+azimjonxolmirzayev@users.noreply.github.com>
2024-08-24 19:04:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://foodmenus.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
